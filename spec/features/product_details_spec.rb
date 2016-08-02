5d2beb07da4910135cc74e0e339887233f21d1cd
munens <kaumbuthom@gmail.com>
2016-08-02 21:35:20 +0000

    Added feature spec checking whether product details page can be acecessed from the root

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Navigate to the product details page", type: :feature, js: true do
+  
+    before :each do
+
+		@category = Category.create! name: 'Apparel'
+
+		1.times do |n|
+			@category.products.create!(
+				name: Faker::Hipster.sentence(3),
+				description: Faker::Hipster.paragraph(3),
+				image: open_asset('apparel1.jpg'),
+				quantity: 10,
+				price: 64.99
+			)
+		end
+
+	end
+
+	scenario "Navigate to the product details page" do
+
+		# ACT
+		visit root_path
+
+		puts page.html
+
+		# click_link('.actions a .btn.btn-default.pull-right')
+		click_on 'Details'
+
+		sleep 3
+		save_screenshot
+
+		# puts page.html
+
+		
+
+
+		expect(page.has_selector?('main section.products-show')).to eq(true)
+
+	end
+
+
 end
