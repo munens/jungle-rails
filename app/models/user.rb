425c91c79ddf69682e555a8a6527cb0e248fb697
munens <kaumbuthom@gmail.com>
2016-10-07 02:22:54 +0000

    changed paaword to password_digest in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false}
-  validates :password, length: {minimum: 8}
+  validates :password_digest, length: {minimum: 8}
 
   def self.authenticate_with_credentials(email, password)
   	
